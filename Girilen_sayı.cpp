4d94c5c254166546b1eff68f5061670bdbb56d27
python533 <143389231+python533@users.noreply.github.com>
2024-03-14 10:56:20 +0300

    C++ Girilen Sayı
    Girilen Sayı Aralğı kadar Toplam

---
+++ Girilen_sayı.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <conio.h>
+#include <string>
+
+int main(){
+int n,toplam=0;
+
+std::cout << "Bir Sayı Girin:" << '\n';
+cin>>n;
+
+
+for ( i = 0; i < len(n); i++) {
+i += toplam;
+std::cout << "Girilen Sayıların Aralğındaki  Toplam" + toplam<< '\n';
+}
+
+
+
+}
